35cfd9f3895cb42f649f8b47a5ca78c50e5a1a1b
MSanouvo <43829596+MSanouvo@users.noreply.github.com>
2025-02-25 00:37:07 -0800

    fix: added correct styling format to btn component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import styles from "./button.module.css"
 
 function Button(props) {
-  return <button onClick={props.handleClick}>{props.name}</button>;
+  return <button className={styles.button} onClick={props.handleClick}>{props.name}</button>;
 }
 
 Button.propTypes = {
